7794523df70801be3e2368191b5907a4bb6206e1
Sameer Kumar <kumar.sam1612@gmail.com>
2023-10-03 14:50:20 +0530

    remapped conflicting codium shortcut

--- .config/nvim/lua/sameer/plugins/lsp/codium.lua
+++ .config/nvim/lua/sameer/plugins/lsp/codium.lua
@@ -2,10 +2,10 @@ return {
   "Exafunction/codeium.vim",
   config = function()
     -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set("i", "<C-g>", function()
+    vim.keymap.set("i", "<C-;>", function()
       return vim.fn["codeium#Accept"]()
     end, { expr = true })
-    vim.keymap.set("i", "<c-;>", function()
+    vim.keymap.set("i", "<c-n>", function()
       return vim.fn["codeium#CycleCompletions"](1)
     end, { expr = true })
     vim.keymap.set("i", "<c-,>", function()
