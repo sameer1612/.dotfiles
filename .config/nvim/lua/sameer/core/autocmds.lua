cd89f11888c9ef66352028ebfb3aa8edfbb69aa3
Sameer Kumar <kumar.sam1612@gmail.com>
2023-10-31 18:52:12 +0530

    added autocommand to save on leaving buffer

--- .config/nvim/lua/sameer/core/autocmds.lua
+++ .config/nvim/lua/sameer/core/autocmds.lua
@@ -2,3 +2,5 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = { "*" },
   command = "normal zR",
 })
+
+vim.api.nvim_create_autocmd({ "BufLeave" }, { pattern = "*", command = "wa", nested = true })
