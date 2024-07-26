27e380d519e8c29f121e400aa1767658d01e98d0
Sameer Kumar <kumar.sam1612@gmail.com>
2024-07-26 19:55:22 +0530

    migrated to harpoon2

--- .config/nvim/lua/sameer/plugins/harpoon.lua
+++ .config/nvim/lua/sameer/plugins/harpoon.lua
@@ -1,45 +1,39 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
+    local harpoon = require("harpoon")
     local keymap = vim.keymap
 
-    keymap.set(
-      "n",
-      "<leader>hh",
-      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
-      { desc = "Go to harpoon menu" }
-    )
-    keymap.set(
-      "n",
-      "<leader>hm",
-      "<cmd>lua require('harpoon.mark').add_file()<cr>",
-      { desc = "Mark file with harpoon" }
-    )
-    keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
-    keymap.set(
-      "n",
-      "<leader>hp",
-      "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
-    keymap.set(
-      "n",
-      "<leader>1",
-      "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
-      { desc = "Go to first harpoon mark" }
-    )
-    keymap.set(
-      "n",
-      "<leader>2",
-      "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
-      { desc = "Go to second harpoon mark" }
-    )
-    keymap.set(
-      "n",
-      "<leader>3",
-      "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
-      { desc = "Go to third harpoon mark" }
-    )
+    harpoon:setup()
+
+    keymap.set("n", "<leader>hh", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Go to harpoon menu" })
+
+    keymap.set("n", "<leader>hm", function()
+      harpoon:list():add()
+    end, { desc = "Mark file with harpoon" })
+
+    keymap.set("n", "<leader>hn", function()
+      harpoon:list():next()
+    end, { desc = "Go to next harpoon mark" })
+
+    keymap.set("n", "<leader>hp", function()
+      harpoon:list():prev()
+    end, { desc = "Go to previous harpoon mark" })
+
+    keymap.set("n", "<leader>1", function()
+      harpoon:list():select(1)
+    end, { desc = "Go to first harpoon mark" })
+
+    keymap.set("n", "<leader>2", function()
+      harpoon:list():select(2)
+    end, { desc = "Go to second harpoon mark" })
+
+    keymap.set("n", "<leader>3", function()
+      harpoon:list():select(3)
+    end, { desc = "Go to third harpoon mark" })
   end,
 }
