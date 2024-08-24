338138edbd14f6308a43b80a57cc059cd701f3fd
Sameer Kumar <kumar.sam1612@gmail.com>
2024-08-24 21:43:06 +0530

    reduced workspace indicator to only show current workspace name

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
-else
-    sketchybar --set $NAME background.drawing=off
-fi
+sketchybar --set $NAME label="$FOCUSED_WORKSPACE"
