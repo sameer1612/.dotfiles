e231e6c0e2fa74643fcc4a06a9e299738c06d27e
Sameer Kumar <kumar.sam1612@gmail.com>
2024-10-08 20:22:11 +0530

    added ivy rich, which key and rainbow delimiters

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -24,9 +24,15 @@
          ("C-d" . ivy-reverse-i-search-kill))
   :init (ivy-mode 1))
 
+(use-package ivy-rich
+  :after ivy
+  :init
+  (ivy-rich-mode 1))
+
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
          ("C-x b" . counsel-ibuffer)
+         ("C-b" . counsel-switch-buffer)
          ("C-x C-f" . counsel-find-file)
          :map minibuffer-local-map
          ("C-r" . 'counsel-minibuffer-history)))
@@ -35,6 +41,16 @@
   :ensure t
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
+
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (load (locate-user-emacs-file "theme.el"))
 (load (locate-user-emacs-file "options.el"))
--- .config/emacs/options.el
+++ .config/emacs/options.el
@@ -3,6 +3,15 @@
 (global-display-line-numbers-mode t)
 
 (setq inhibit-startup-message t)
-(setq display-line-numbers-type 'relative)
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode nil)
+
+
+(setq display-line-numbers-type 'relative)
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
