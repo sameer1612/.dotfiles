df00de036d695e4d93f54179500d54ca9b2a301a
Sameer Kumar <kumar.sam1612@gmail.com>
2024-10-12 19:40:27 +0530

    deleted emacs backup

--- .config/emacs.backup/init.el
+++
@@ -1,7 +0,0 @@
-(load (locate-user-emacs-file "packages/packages.el"))
-
-(load (locate-user-emacs-file "theme.el"))
-(load (locate-user-emacs-file "options.el"))
-
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(load custom-file 'noerror 'nomessage)
