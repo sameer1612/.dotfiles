6da220dc3f36c3ef1a04ed98ecc73ecef3693772
Sameer Kumar <kumar.sam1612@gmail.com>
2024-10-13 18:01:31 +0530

    removed mason

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -2,11 +2,41 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls" }
+local servers =
+  { "html", "cssls", "ts_ls", "tailwindcss", "angularls", "emmet_ls", "pyright", "ruby_lsp", "graphql", "elixirls" }
 local nvlsp = require "nvchad.configs.lspconfig"
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap
 
 local on_attach = function(client, bufnr)
   nvlsp.on_attach(client, bufnr)
+
+  opts.buffer = bufnr
+
+  opts.desc = "Show LSP references"
+  keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+
+  opts.desc = "Go to declaration"
+  keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+  opts.desc = "Show LSP definitions"
+  keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+
+  opts.desc = "Show LSP implementations"
+  keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+
+  opts.desc = "Show LSP type definitions"
+  keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+  opts.desc = "Smart rename"
+  keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
+
+  opts.desc = "Show buffer diagnostics"
+  keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+
+  opts.desc = "Restart LSP"
+  keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
 end
 
 -- lsps with default config
