c2d1bafe1b2c0158026c8cabdbaa5c9f0de4ea4b
Sameer Kumar <kumar.sam1612@gmail.com>
2024-11-15 14:05:29 +0530

    added abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,28 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# abbreviations
+abbr -a -- gst "git status"
+abbr -a -- gc "git branch | fzf --height=20% --reverse --info=inline | xargs git checkout"
+abbr -a -- gpull "git pull"
+abbr -a -- gpush "git push"
+abbr -a -- rserver "rails s "
+abbr -a -- rconsole "rails c "
+abbr -a -- yd "yarn dev || yarn start "
+abbr -a -- yt "yarn test"
+abbr -a -- yb "yarn build"
+abbr -a -- ys "yarn start"
+abbr -a -- python "python3"
+abbr -a -- pip "pip3"
+abbr -a -- nv "nvim"
+abbr -a -- vi "nvim"
+abbr -a -- v "nvim"
+abbr -a -- ls "eza"
+abbr -a -- tr2 "tree -a -L 2"
+abbr -a -- cls "clear"
+abbr -a -- l "ls -l"
+abbr -a -- la "ls -la"
+abbr -a -- lf "yazi"
+abbr -a -- lfcd "y"
+
