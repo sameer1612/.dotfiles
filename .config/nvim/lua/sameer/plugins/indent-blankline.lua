58ca11c795b7b8d3edbfe2aeaf11f289e2adbfb1
Sameer Kumar <kumar.sam1612@gmail.com>
2025-08-04 17:51:57 +0530

    added indent blankline and mini nvim

---
+++ .config/nvim/lua/sameer/plugins/indent-blankline.lua
@@ -0,0 +1,7 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  event = { "BufReadPre", "BufNewFile" },
+  config = true
+}
+
---
+++ .config/nvim/lua/sameer/plugins/mini.lua
@@ -0,0 +1,6 @@
+return {
+  "echasnovski/mini.nvim",
+  config = function()
+    require("mini.statusline").setup()
+  end,
+}
