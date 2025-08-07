674eb78495c0747b11fe58e8821e087ee9625fa7
Sameer Kumar <kumar.sam1612@gmail.com>
2025-08-07 20:07:26 +0530

    Switch to Gruvbox Material theme for Kitty and Neovim
    Replaced Everforest and Doom One themes with Gruvbox Material in Kitty and Neovim configurations. Updated theme plugin and settings in Neovim, changed theme includes in Kitty configs, and removed a temporary Yazi config file.

--- .config/nvim/lua/sameer/plugins/theme.lua
+++ .config/nvim/lua/sameer/plugins/theme.lua
@@ -1,7 +1,14 @@
 return {
-  "neanias/everforest-nvim",
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
   config = function()
-    require("everforest").setup()
-    vim.cmd("colorscheme everforest")
+    vim.g.gruvbox_material_background = "medium"
+    vim.g.gruvbox_material_enable_italic = 0
+    vim.g.gruvbox_material_disable_italic_comment = 1
+    vim.g.gruvbox_material_better_performance = 1
+
+    vim.o.background = "dark"
+    vim.cmd("colorscheme gruvbox-material")
   end,
 }
