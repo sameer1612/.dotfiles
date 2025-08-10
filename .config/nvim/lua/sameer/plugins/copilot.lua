8abfedbc66922b119840ba602de5f83bfe73262f
Sameer Kumar <kumar.sam1612@gmail.com>
2025-08-10 17:47:28 +0530

    Add keymap for Copilot suggestion handling
    Introduced <C-;> keybinding in insert mode to accept Copilot suggestions if visible, or move to the next suggestion otherwise. This improves workflow efficiency when using Copilot in Neovim.

--- .config/nvim/lua/sameer/plugins/copilot.lua
+++ .config/nvim/lua/sameer/plugins/copilot.lua
@@ -11,5 +11,13 @@ return {
         auto_refresh = true,
       },
     })
+
+    vim.keymap.set("i", "<C-;>", function()
+      if require("copilot.suggestion").is_visible() then
+        require("copilot.suggestion").accept()
+      else
+        require("copilot.suggestion").next()
+      end
+    end, { desc = "Accept Copilot suggestion or go to next" })
   end,
 }
