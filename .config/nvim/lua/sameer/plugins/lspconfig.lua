262b6ab7d3e12bf3b9decc81e50e155938bca50c
Sameer Kumar <kumar.sam1612@gmail.com>
2025-08-13 21:05:31 +0530

    added lazydev for vim global support

--- .config/nvim/lua/sameer/plugins/lspconfig.lua
+++ .config/nvim/lua/sameer/plugins/lspconfig.lua
@@ -1,6 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
+    {
+      "folke/lazydev.nvim",
+      ft = "lua",
+      opts = {
+        library = {
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
     "saghen/blink.cmp",
     {
       "mason-org/mason.nvim",
